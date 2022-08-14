d1176fd03a9a2fd92efad5738fc736bab0b23f79
Marycha <64fauchenicolas@gmail.com>
2022-08-14 15:57:40 +0200

    firt commit

---
+++ De_magique.ino
@@ -0,0 +1,22 @@
+/*
+ Name:		De_magique.ino
+ Created:	14/08/2022 15:09:30
+ Author:	fauch
+*/
+
+// the setup function runs once when you press reset or power the board
+
+// DEux paire de fichier: les selecteurs , et le display
+
+void setup() {
+	// il faut :
+	// Un switch selection nombre de de
+	// Un ecran lcd import de la Lib lcd 
+	// un bouton poussoir 
+	// Un variateur selction du nombre de 
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
